ada2eb05e4f7c9bfb8fa7180a0e9a26a8d091438
Yi Zhang <yi16@ualberta.ca>
2017-09-30 14:41:26 -0600

    1
    v2.0

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,9 +13,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String FILENAME = "file.sav";
     ListView listView;
     private ArrayList<Counter> counters = new ArrayList<>();
     private ArrayAdapter<Counter> mAdapter;
@@ -24,21 +37,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        counters.add(new Counter("counter1", 0, "test!!!"));
+        /*counters.add(new Counter("counter1", 0, "test!!!"));
         counters.add(new Counter("counter2", 0));
         counters.add(new Counter("counter3", 0));
         counters.add(new Counter("counter4", 0));
         counters.add(new Counter("counter5", 0));
         counters.add(new Counter("counter6", 0));
-
+*/
         listView = (ListView) findViewById(R.id.listView);
 
         /*ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.countries));*/
-        mAdapter = new ArrayAdapter<Counter>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                counters);
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -54,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,1);
             }
         });
-        listView.setAdapter(mAdapter);
+
         Button add = (Button) findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -75,12 +86,14 @@ public class MainActivity extends AppCompatActivity {
                 counters.get(counterIndex).setInitialValue(data.getIntExtra("initialValue", -1));
                 counters.get(counterIndex).setComment(comment);
                 counters.get(counterIndex).setDate();
+                saveInFile();
 
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //delete current counter
                 counters.remove(data.getIntExtra("counterIndex",-1));
                 mAdapter.notifyDataSetChanged();
+                saveInFile();
             }
         }
         else if (requestCode == 2){
@@ -89,7 +102,59 @@ public class MainActivity extends AppCompatActivity {
                         data.getIntExtra("initialValue",-1),
                         data.getStringExtra("comment")));
                 mAdapter.notifyDataSetChanged();
+                saveInFile();
             }
         }
     }
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        loadFromFile();
+        mAdapter = new ArrayAdapter<Counter>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                counters);
+        listView.setAdapter(mAdapter);
+    }
+    private void loadFromFile() {
+        //ArrayList<String> tweets = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Counter>>() {}.getType();
+            counters = gson.fromJson(in, listType);
+
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            counters = new ArrayList<Counter>();
+            //e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException(e);
+            //e.printStackTrace();
+        }
+        //return tweets.toArray(new String[tweets.size()]);
+    }
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(counters, writer);
+            writer.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+            //e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+            //e.printStackTrace();
+        }
+    }
+
 }
