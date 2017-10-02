0e2d6aa984323292176e1ecaefd74625f4898bc5
Yi Zhang <yi16@ualberta.ca>
2017-10-01 23:22:10 -0600

    1
    11

--- app/src/main/java/com/example/test/Counter.java
+++ app/src/main/java/com/example/test/Counter.java
@@ -5,7 +5,11 @@ import java.util.Date;
 /**
  * Created by Yi on 2017/9/29.
  */
-
+/*
+* Counter class contains all data of a counter and all
+* method to construct and interact with it
+*
+* */
 public class Counter {
 
     private String name;
@@ -13,13 +17,6 @@ public class Counter {
     private int currentValue;
     private Date date;
     private String comment;
-
-    public Counter(String name, int initialValue){
-        this.name = name;
-        this.initialValue = initialValue;
-        this.currentValue = initialValue;
-        this.date = new Date();
-    }
 
     public Counter(String name, int initialValue, String comment){
         this.name = name;
